2281eda6fb376d02e02e6bc53767e2649cea6861
harsh johar <hpsj3112@gmail.com>
2022-12-15 03:19:32 +0530

    Added the basic files for the project.

---
+++ slowmokit/core.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file core.hpp
+ *
+ * Include all of the base components required to write slowmokit methods.
+ */
+
+#ifndef SLOWMOKIT_CORE_HPP
+#define SLOWMOKIT_CORE_HPP
+
+// pre-requisites of the library
+#include "prereqs.hpp"
+
+// standard model class
+#include "models/model.hpp"
+
+#endif //SLOWMOKIT_CORE_HPP
