354996d02b215da4af6cb9f79713e95b5a694f1f
harsh johar <hpsj3112@gmail.com>
2022-12-15 15:39:10 +0530

    CMAKE setup and base files

---
+++ slowmokit/neighbors/knn/knn.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file neighbors/knn/knn.cpp
+ *
+ * Implementation of the KNN main program
+ */
+#include "knn.hpp"
+
+template <typename T>
+class KNN : public Model<T> {
+
+};
---
+++ slowmokit/neighbors/knn/knn.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file neighbors/knn/knn.hpp
+ *
+ * The header file including the knn
+ */
+#ifndef SLOWMOKIT_KNN_HPP
+#define SLOWMOKIT_KNN_HPP
+
+#include "../../models/model.hpp"
+
+#endif //SLOWMOKIT_KNN_HPP
