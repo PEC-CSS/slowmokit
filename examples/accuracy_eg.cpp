77be02b561e458981230d541ee1ed3a02b49f793
Modernbeast02 <89138051+Modernbeast02@users.noreply.github.com>
2023-02-06 23:03:52 +0530

    Delete accuracy_eg.cpp

--- examples/accuracy_eg.cpp
+++
@@ -1,9 +0,0 @@
-// #include "../src/slowmokit/methods/metrics/accuracy.hpp"
-
-// int main() {
-//   std::vector<int> pred = {1, 0, 1, 1, 0, 1};
-//   std::vector<int> trueLabels = {1, 0, 0, 1, 0, 1};
-//   double acc = accuracy(pred, trueLabels);
-//   std::cout << "Accuracy: " << acc << std::endl;
-//   return 0;
-// }
