8866ec92938cc6f487528ebfe7eb7f8f0aa0d971
Modernbeast02 <89138051+Modernbeast02@users.noreply.github.com>
2023-02-07 00:30:25 +0530

    Update classification_report.hpp

--- src/slowmokit/methods/metrics/classification_report.hpp
+++ src/slowmokit/methods/metrics/classification_report.hpp
@@ -8,6 +8,14 @@
 #define SLOWMOKIT_CLASSIFICATION_REPORT_HPP
 #include "../../core.hpp"
 
+/**
+ * Takes predicted and actual values of classes
+ * @param predictedValue -> predicted values 
+ * @param trueValue -> true values
+ * @returns the classification report
+ * @throws domain_error exception when size of the two vectors is not equal
+ */
+
 template <class T>
 class ClassificationReport
 {
@@ -19,17 +27,45 @@ private:
 public:
     ClassificationReport(std::vector<T> &x, std::vector<T> &y);
 
+    /**
+     * @throws domain_error exception when size of the two vectors is not equal
+     */
     void confusionMatrix(std::vector<T> &, std::vector<T> &);
 
+
+    /**
+     * @finding precision values for all the classes 
+     * @returns the map with all the precision values
+     */
     std::map<T, double> precision();
 
+
+    /**
+     * @finding recall values for all the classes 
+     * @returns the map with all the recall values
+     */
     std::map<T, double> recall();
 
-    std::map<T, double> f1_score();
 
+    /**
+     * @finding f1scores value for all the classes 
+     * @returns the map with all the f1scores values
+     */
+    std::map<T, double> f1Score();
+
+
+    /**
+     * @finding accuracy  for all the classes 
+     * @returns the map with all the accuracy values
+     */
     std::map<T, double> accuracy();
 
-    void PrintReport();
+
+    /**
+     * @printing all the report paramenters in the form of table 
+     * @returns nothing except happiness 
+     */
+    void printReport();
 };
 
 #endif // SLOWMOKIT_CLASSIFICATION_REPORT_HPP
