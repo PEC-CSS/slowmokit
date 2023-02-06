18a6fe124bad231183662f22a4620b0f1d2f1f66
SARTHAK <shibugarg0303@gmail.com>
2023-02-06 14:32:23 +0530

    Comments of functions

--- src/slowmokit/methods/neighbors/bernoulli_nb/bernoulli_nb.hpp
+++ src/slowmokit/methods/neighbors/bernoulli_nb/bernoulli_nb.hpp
@@ -12,13 +12,25 @@
 template<class T>
 class BernoulliNB{  
 private:
-    // returns prior probability
+    /*
+    @brief Function Prior probability
+    @param: yTrain to compute P(y=y1) and label: y1
+    @returns double value of prior Probability
+    */
     double priorProb(std::vector<T> yTrain,int label);
-    // return conditional probability
+    /*
+    @brief Function conditional Probabilty
+    @param: xTrain: all x training values , yTrain: all y training values, featureCol: for which feature column we are finding conditional prob. ; featureVal: for what value in that feature we are looking for conditional prob.; label: y value looking for
+    @returns double value of conditional probability
+    */
     double conditionalProb(std::vector<std::vector<T>> xTrain,std::vector<T> yTrain,int featureCol,int featureVal,int label);
 
 public:
-    // returns predicted value for each test according to bernoulli naive bayes model
+    /*
+    brief Function fit and predict the output
+    @param: xTrain: training values, yTrain: y training values, xTest: testing values
+    @returns returns predicted value for each test according to bernoulli naive bayes model
+    */
     int fitPredict(std::vector<std::vector<T>> xTrain,std::vector<T> yTrain,std::vector<T> xTest);
 };
 
