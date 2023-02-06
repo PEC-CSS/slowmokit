4a69adc3b488993b217f2269f320ae05585e8a0f
SARTHAK <shibugarg0303@gmail.com>
2023-02-06 17:16:24 +0530

    params

--- src/slowmokit/methods/neighbors/bernoulli_nb/bernoulli_nb.hpp
+++ src/slowmokit/methods/neighbors/bernoulli_nb/bernoulli_nb.hpp
@@ -12,24 +12,30 @@
 template<class T>
 class BernoulliNB{  
 private:
-    /*
+    /**
     @brief Function Prior probability
-    @param: yTrain to compute P(y=y1) and label: y1
+    @param yTrain to compute P(y=y1) and label y1
     @returns double value of prior Probability
     */
     double priorProb(std::vector<T> yTrain,int label);
-    /*
+    /**
     @brief Function conditional Probabilty
-    @param: xTrain: all x training values , yTrain: all y training values, featureCol: for which feature column we are finding conditional prob. ; featureVal: for what value in that feature we are looking for conditional prob.; label: y value looking for
+    @param xTrain 2-d all x training values
+    @param yTrain all 1-d y training values
+    @param featureCol for which feature column we are finding conditional prob
+    @param featureVal for what value in that feature we are looking for conditional prob
+    @param label y value looking for
     @returns double value of conditional probability
     */
     double conditionalProb(std::vector<std::vector<T>> xTrain,std::vector<T> yTrain,int featureCol,int featureVal,int label);
 
 public:
-    /*
-    brief Function fit and predict the output
-    @param: xTrain: training values, yTrain: y training values, xTest: testing values
-    @returns returns predicted value for each test according to bernoulli naive bayes model
+    /**
+    @brief Function fit and predict the output
+    @param xTrain 2-d training values
+    @param yTrain 1-d y training values
+    @param xTest testing values
+    @returns predicted value for each test according to bernoulli naive bayes model
     */
     int fitPredict(std::vector<std::vector<T>> xTrain,std::vector<T> yTrain,std::vector<T> xTest);
 };
