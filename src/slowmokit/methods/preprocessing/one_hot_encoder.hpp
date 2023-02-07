af5b1dd0279a8254a203ec4ff53c1991dc5445a7
NandiniGera <nandinigera.bt21cse@pec.edu.in>
2023-02-07 15:00:40 +0530

    updated

--- src/slowmokit/methods/preprocessing/one_hot_encoder.hpp
+++ src/slowmokit/methods/preprocessing/one_hot_encoder.hpp
@@ -16,6 +16,6 @@
  */
 
 template<class T>
-std::vector<std::vector<int>> oneHotEncoder(const std::vector<int> &, int );
+std::vector<std::vector<int>> oneHotEncoder(const std::vector<T> &, int );
 
 #endif // ONE_HOT_ENCODER_HPP
