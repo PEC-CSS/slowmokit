729c3ab1cc99b2e6a3e2ebf509f644716a38bc73
YASHITA BANSAL <96726018+YashitaBansal@users.noreply.github.com>
2023-02-07 21:08:19 +0530

    Create recall.cpp

---
+++ src/slowmokit/methods/metrics/recall.cpp
@@ -0,0 +1,54 @@
+/**
+ * @file methods/metrics/recall.cpp
+ *
+ * Implementation of the recall main program
+ */
+#include "recall.hpp"
+template <class T>
+std::map<T, double> recall(std::vector<T> &pred, std::vector<T> &actual)
+{
+    if (pred.size() != actual.size())
+    {
+        throw std::invalid_argument("Predicted and actual vectors must have same size");
+    }
+    int n = actual.size();
+    std::set<T> s;
+    for (int i = 0; i < n; i++)
+    {
+        s.insert(actual[i]);
+    }
+    int numClasses=s.size();
+    std::map<T, double> recallMap;
+    std::map<T, int> truePosMap, falseNegMap;
+    for (int i = 0; i < n; i++)
+    {
+        if (pred[i] == actual[i])
+        {
+            truePosMap[actual[i]]++;
+        }
+        else
+        {
+            falseNegMap[actual[i]]++;
+        }
+    }
+
+    for (int i=0;i<numClasses;i++)
+    {
+       if (truePosMap[i] > 0 || falseNegMap[i] > 0)
+        {
+            recallMap[i] = (double)(truePosMap[i] / (double)(truePosMap[i] + falseNegMap[i]));
+        }
+        else
+        {
+            recallMap[i] = 0.0;
+        }
+
+        double x = recallMap[i];
+        float value = (int)(x * 100 + .5);
+        recallMap[i] = (float)value / 100;
+    }
+
+
+    return recallMap;
+}
+
