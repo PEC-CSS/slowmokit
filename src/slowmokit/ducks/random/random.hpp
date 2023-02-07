6f097130b0887c82098526839d537d1120d423dd
Harshpreet Singh Johar <50266759+harshjohar@users.noreply.github.com>
2023-02-08 00:50:52 +0530

    Cpp linter (#78)
    * ready to push ig
    
    * LINTER
    
    * linter
    
    * linter
    
    * we dont need this
    
    * test cfloat
    
    * limit.h removed
    
    * redundent workflow
    
    * again
    
    * maybe now?
    
    * testing>
    
    * script ready!
    
    * readme update for format
    
    * remove dev branch

--- src/slowmokit/ducks/random/random.hpp
+++ src/slowmokit/ducks/random/random.hpp
@@ -10,38 +10,37 @@
 #include "../../core.hpp"
 
 /**
- * @brief Function to get a real random number between two numbers L and R inclusive
- * @param L: Starting element of the range 
- * @param R: Ending element of the range 
+ * @brief Function to get a real random number between two numbers L and R
+ * inclusive
+ * @param L: Starting element of the range
+ * @param R: Ending element of the range
  * @returns a random number between L and R inclusive
  * @throws std::domain_error if L or R are not floating point type
  * @throws std::length_error exception if R > L
  */
-template <class RealType> 
-RealType random(RealType, RealType);
+template<class RealType> RealType random(RealType, RealType);
 
 /**
- * @brief Function to get a real random number between two numbers L and R inclusive
- * @param L: Starting element of the range 
- * @param R: Ending element of the range 
+ * @brief Function to get a real random number between two numbers L and R
+ * inclusive
+ * @param L: Starting element of the range
+ * @param R: Ending element of the range
  * @returns a random number between L and R inclusive
  * @throws std::domain_error if L or R is not signed/unsigned int or char
  * @throws std::length_error exception if R > L
  */
-template <class IntType>
-IntType randint(IntType, IntType);
+template<class IntType> IntType randint(IntType, IntType);
 
 /**
  * @brief seed for generating
  */
-std::seed_seq seed
-{
-    (uint32_t)std::chrono::duration_cast<std::chrono::nanoseconds>
-        (std::chrono::high_resolution_clock::now().time_since_epoch()).count(),
-    (uint32_t)std::random_device()(),
-    (uint32_t)(uintptr_t)std::make_unique<char>().get(),
-    (uint32_t)__builtin_ia32_rdtsc()
-};
+std::seed_seq seed {
+    (uint32_t) std::chrono::duration_cast<std::chrono::nanoseconds>(
+        std::chrono::high_resolution_clock::now().time_since_epoch())
+        .count(),
+    (uint32_t) std::random_device()(),
+    (uint32_t) (uintptr_t) std::make_unique<char>().get(),
+    (uint32_t) __builtin_ia32_rdtsc()};
 
 
 #endif // SLOWMOKIT_RANDOM_HPP
