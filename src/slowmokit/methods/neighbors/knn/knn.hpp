9035d2d5050d1920bedc1e8c8f585e4b1939b3d3
SARTHAK <shibugarg0303@gmail.com>
2023-02-07 09:03:18 +0530

    Comments added in KNN

--- src/slowmokit/methods/neighbors/knn/knn.hpp
+++ src/slowmokit/methods/neighbors/knn/knn.hpp
@@ -15,9 +15,30 @@ private:
     std::vector<int> yTrain;
     int k=1;
     int classNums=1;
+    /**
+     * @brief Calculates distance euclidean/else
+     * 
+     * @param x1 first point
+     * @param x2 second point
+     * @return double value of distance
+     */
     double distance(std::vector<T> x1,std::vector<T> x2);
 public:
+    /**
+     * @brief store values of training data
+     * 
+     * @param x all x training values
+     * @param y all y training values
+     * @param k minimum number of nearest neighbors
+     * @param classNums number of classes
+     */
     void fit(std::vector<std::vector<T>> x,std::vector<int> y,int k,int classNums);
+    /**
+     * @brief predict to which cluster it belongs
+     * 
+     * @param test testing value
+     * @return int cluster number to which it belongs
+     */
     int predict(std::vector<T> test);
 };
 
