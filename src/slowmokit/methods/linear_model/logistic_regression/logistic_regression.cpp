f474759ab8bfabbb4f2ee36f0520beef89c198bc
SARTHAK <shibugarg0303@gmail.com>
2023-02-08 16:49:32 +0530

    values vector to exps

--- src/slowmokit/methods/linear_model/logistic_regression/logistic_regression.cpp
+++ src/slowmokit/methods/linear_model/logistic_regression/logistic_regression.cpp
@@ -19,7 +19,7 @@ std::vector<double> LogisticRegression<T>::softmax(std::vector<T> x)
   std::vector<double> values(x.size());
   for (int i = 0; i < x.size(); i++)
   { // Now computing actual values after applying softmax
-     exps[i] /= double(sum);
+    exps[i] /= double(sum);
   }
   return exps;
 };
