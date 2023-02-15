d5c782a64a7ce077197b1a24fa7cbcf149d3e7e4
Abhinav Malhotra <113239170+abhinavmalhotra01@users.noreply.github.com>
2023-02-15 22:59:07 +0530

    changes

--- src/slowmokit/methods/neighbors/categorical_nb/categorical_nb.cpp
+++ src/slowmokit/methods/neighbors/categorical_nb/categorical_nb.cpp
@@ -5,17 +5,8 @@
  */
 
 #include "categorical_nb.hpp"
-
-template<class T> std::map<std::string, double> priors;
 template<class T>
-std::map<std::string, std::map<std::pair<T, int>, double>> likelihoods;
-// for each label we will store a map , containing n features
-// and their corresponding probability
-
-int featureSize = 0; // for storing the number of columns(number of features) in
-                     // xTrain and to compare it with the testing data
-template<class T>
-void fit(std::vector<std::vector<T>> xTrain, std::vector<std::string> yTrain)
+void CategoricalNB<T> :: fit(std::vector<std::vector<T>> xTrain, std::vector<std::string> yTrain)
 {
   // posterior = (prior * likelihood)/evidence
   // since, evidence is same among all instances -> we can ignore it
@@ -39,7 +30,7 @@ void fit(std::vector<std::vector<T>> xTrain, std::vector<std::string> yTrain)
   }
   for (auto current : occurences)
   {
-    priors<T>[current.first] =
+    priors[current.first] =
         double(current.second) /
         yTrain.size(); // calculating the priors of each label, dividing the
                        // occurence by the size of the total set
@@ -49,7 +40,7 @@ void fit(std::vector<std::vector<T>> xTrain, std::vector<std::string> yTrain)
       counts; // to store the count of each cell corresponding to it's label,
               // it's row and it's category
 
-  for (int i = 0; i < xTrain.size();
+  for (int i = 0; i < (int)(xTrain.size());
        i++) // iterating over the training data grid
   {
     std::vector<T> current = xTrain[i]; // current row
@@ -69,7 +60,7 @@ void fit(std::vector<std::vector<T>> xTrain, std::vector<std::string> yTrain)
          current.second) // iterating over the map corresponding to each label
                          // to find the likelihood of each entry
     {
-      likelihoods<T>[current.first][{e.first.first, e.first.second}] =
+      likelihoods[current.first][{e.first.first, e.first.second}] =
           ((double(e.second)) / (occurences[current.first]));
       // likelihood[label][current feature]=occ in current/total no of occ
     }
@@ -77,15 +68,16 @@ void fit(std::vector<std::vector<T>> xTrain, std::vector<std::string> yTrain)
 }
 
 template<class T>
-std::string predict(
+
+std::string CategoricalNB<T>:: predict(
     std::vector<T> xTest) // predicting the label on the basis of training set
 {
-  if (xTest.size() != featureSize)
+  if (((int)(xTest.size())) != featureSize)
   {
     throw "The number of features in training and testing set must be same";
   }
   std::map<std::string, double> probs;
-  for (auto curr : priors<T>) // since, posterior = prior*likelihood , we will
+  for (auto curr : priors) // since, posterior = prior*likelihood , we will
                               // calculate the same for each label to give the
                               // label with highest probability as the output
   {
@@ -93,7 +85,7 @@ std::string predict(
     int j = 0;
     for (auto feature : xTest)
     {
-      probs[curr.first] *= likelihoods<T>[curr.first][{
+      probs[curr.first] *= likelihoods[curr.first][{
           feature,
           j}]; // calculating posterior for each feature in Testing data
       j++;
@@ -110,6 +102,8 @@ std::string predict(
       out = prob.first;
     }
   }
-
+  for(auto e : probs){
+    std::cout<<e.first<<" "<<e.second<<std::endl;
+  }
   return out;
 }
